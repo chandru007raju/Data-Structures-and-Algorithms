3f50f0aa57f07eb8dc562b7af7c8f6fd1f9c4ae7
sr.chandrashekar <chandru007.raju@gmail.com>
2023-01-18 05:53:37 +0530

    updated

--- doubly-linkedlist/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-nt();
-    InsertAtHead(3); Print(); ReversePrint();
-    InsertAtHead(4); Print(); ReversePrint();
-    InsertAtHead(5); Print(); ReversePrint();
-    InsertAtHead(6); Print(); ReversePrint();
-}
