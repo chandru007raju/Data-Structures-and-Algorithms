f9d32d5d17e07ddd78bde9952516f3972dbfab4d
sr.chandrashekar <chandru007.raju@gmail.com>
2023-01-31 21:22:55 +0530

    updated

--- linkedlist/1insertbeginning.cpp
+++ linkedlist/1insertbeginning.cpp
@@ -1,40 +1,53 @@
 
 #include<iostream>
-
+#include<string>
+using namespace std;
 
 struct Node
 {
-    int data;
+    int id;
+    string naam;
+    int salary;
     struct Node* link;
     
 };
 struct Node* head; // global variable
-void Insert(int x){
+void Insert(int x,string naam,int y){
     Node* temp = new Node();
-    (*temp).data = x;
+    (*temp).id = x;
+    (*temp).naam = naam;
+    (*temp).salary = y;
     (*temp).link = NULL;
     if(head != NULL) (*temp).link = head;
     head = temp;
 }
 void Print(){
+    
     struct Node* temp = head;
-    std::cout<<"List is : ";
+    std::cout<<"List is : "<<endl;
     while(temp != NULL){
-        std::cout<< (*temp).data << " " ;
+        std::cout<< (*temp).id<< " "<<(*temp).naam << " "<<(*temp).salary ;
         temp = (*temp).link;
+        std::cout << "\n" ;
     }
     std::cout << "\n" ;
 }
 int main()
 {
     head =NULL;
-    std::cout <<"enter how many numbers u want to insert";
-    int n,x;
+    std::cout <<"enter how many numbers u want to insert ";
+    int n,x,y;
+    string naam;
+
     std::cin >> n;
     for (int i = 0; i<n ; i++){
-        std::cout << "enter the number";
+        std::cout << "enter the id ";
         std::cin >> x;
-        Insert(x);
+        std::cout<< "enter tha name ";
+        std::cin>> naam;
+        std::cout << "enter the salary ";
+        std::cin >> y;
+        Insert(x,naam,y);
         Print();
         
     }
